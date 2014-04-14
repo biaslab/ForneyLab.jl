1846197409bea0c5f31535573b4717141c41886b
Thijs van de Laar <thijsvdlaar@hotmail.com>
2014-04-14 16:53:50 +0200

    Added edge type and some rudimentary tests

--- src/ForneyLab.jl
+++ src/ForneyLab.jl
@@ -14,12 +14,14 @@ GaussianMessage() = GaussianMessage([1.0], [0.0])
 type ScalarParameterMessage{T} <: Message
     value::T
 end
+ScalarParameterMessage() = ScalarParameterMessage(1.0)
 
 #
 # Nodes
 #
 abstract Node
 
+# An interface is a half-edge that connects to another interface to form a complete edge.
 type Interface
     node::Node
     partner::Union(Interface, Nothing)
@@ -41,13 +43,28 @@ type MultiplicationNode <: Node
     end
 end
 
+# Combine two interfaces to form a complete edge.
+type Edge
+    tail::Union(Interface, Nothing)
+    head::Union(Interface, Nothing)
+    # TODO: Constructor
+end
+
+#
+# calculatemessage calculates the outgoing message on the sink interface (half-edge).
+# Every combination of node and message types will get an individual signature for the function call.
+#
 function calculatemessage(  sink::Interface,
                             node::MultiplicationNode,
                             multiplierMessage::ScalarParameterMessage,
                             sourceMessage::GaussianMessage)
-    sink.message = GaussianMessage()
+    # Multiplication of a Gaussian message
+    sink_V = multiplierMessage.value' * sourceMessage.V * multiplierMessage.value
+    sink_m = multiplierMessage.value * sourceMessage.m
+    sink.message = GaussianMessage(sink_V, sink_m)
 end
 
+# call for calculatemessage that calculates the incoming messages by recursion.
 function calculatemessage(  sink::Interface,
                             node::MultiplicationNode)
     # TODO: Collect all incoming messages
@@ -55,5 +72,8 @@ function calculatemessage(  sink::Interface,
 end
 
 calculatemessage(sink::Interface) = calculatemessage(sink, sink.node)
+
+calculateforwardmessage(edge::Edge) = calculatemessage(edge.tail)
+calculatebackwardmessage(edge::Edge) = calculatemessage(edge.head)
 
 end # module ForneyLab
--- test/test_forneylab.jl
+++ test/test_forneylab.jl
@@ -3,4 +3,34 @@ module TestForneyLab
 using FactCheck
 using ForneyLab
 
+context("Messages") do
+	facts("It should initialize gaussian messages") do
+		@fact ForneyLab.GaussianMessage().V => [1.0]
+		@fact ForneyLab.GaussianMessage().m => [0.0]
+	end
+
+	facts("It should initiatize a multiplication parameter as message") do
+		@fact ForneyLab.ScalarParameterMessage(1.0).value => 1.0
+		@fact ForneyLab.ScalarParameterMessage([1.0, 2.0]).value => [1.0, 2.0]
+	end
+end
+
+context("Nodes") do
+	facts("It should initialize multiplication nodes") do
+		@fact typeof(ForneyLab.MultiplicationNode()) => ForneyLab.MultiplicationNode
+		@fact ForneyLab.MultiplicationNode().multiplier.message => nothing
+	end
+end
+
+context("Edges") do
+	facts("It should initialize interfaces as half-edges on a node") do
+	end
+
+	facts("It should initiatize edges as a combination of two interfaces") do
+	end
+end
+
+context("calculatemessage") do
+end
+
 end # module TestForneyLab
