410c2ab5b21c8a4981c176f44763e7110e29f142
Marco Cox <mail@marcocox.com>
2014-04-14 15:46:03 +0200

    Some first code to test functionality.

--- src/ForneyLab.jl
+++ src/ForneyLab.jl
@@ -11,6 +11,10 @@ type GaussianMessage <: Message
 end
 GaussianMessage() = GaussianMessage([1.0], [0.0])
 
+type ScalarParameterMessage{T} <: Message
+    value::T
+end
+
 #
 # Nodes
 #
@@ -18,20 +22,38 @@ abstract Node
 
 type Interface
     node::Node
-    partner::Interface
+    partner::Union(Interface, Nothing)
+    message::Union(Message, Nothing)
 end
+Interface(node::Node) = Interface(node, nothing, nothing)
 
-# Mulitiply with parameter a: dst = a * src
+# Mulitiply source with parameter multiplier: sink = multiplier * source
 type MultiplicationNode <: Node
-    a::Interface
-    src::Interface
-    dst::Interface
+    multiplier::Interface
+    source::Interface
+    sink::Interface
     function MultiplicationNode()
         self = new()
-        a = Interface(self)
-        src = Interface(self)
-        dst = Interface(self)
+        self.multiplier = Interface(self)
+        self.source = Interface(self)
+        self.sink = Interface(self)
+        return self
     end
 end
+
+function calculatemessage(  sink::Interface,
+                            node::MultiplicationNode,
+                            multiplierMessage::ScalarParameterMessage,
+                            sourceMessage::GaussianMessage)
+    sink.message = GaussianMessage()
+end
+
+function calculatemessage(  sink::Interface,
+                            node::MultiplicationNode)
+    # TODO: Collect all incoming messages
+    calculatemessage(sink, node, ScalarParameterMessage(4.0), GaussianMessage())
+end
+
+calculatemessage(sink::Interface) = calculatemessage(sink, sink.node)
 
 end # module ForneyLab
