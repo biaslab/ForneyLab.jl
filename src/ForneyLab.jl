7d5ee801d4cf259cc103dae1332ef8a44b1151c1
Marco Cox <mail@marcocox.com>
2014-04-15 16:06:51 +0200

    Work in progress.

--- src/ForneyLab.jl
+++ src/ForneyLab.jl
@@ -29,6 +29,29 @@ type Interface
 end
 Interface(node::Node) = Interface(node, nothing, nothing)
 
+# interfaces(node) returns an array of interfaces for iterating
+function interfaces(node::Node)
+    node_interfaces = Array(Interface,0)
+    for field in names(node)
+        if typeof(getfield(node, field)) == Interface
+            node_interfaces = [node_interfaces, getfield(node, field)]
+        end
+    end
+
+    return node_interfaces
+end
+
+# Constant node
+type ConstantNode <: Node
+    constant::Float64
+    interface::Interface
+    function ConstantNode(constant)
+        self = new(constant)
+        self.interface = Interface(self)
+        return self
+    end
+end
+
 # Mulitiply source with parameter multiplier: sink = multiplier * source
 type MultiplicationNode <: Node
     multiplier::Interface
@@ -45,14 +68,13 @@ end
 
 # Combine two interfaces to form a complete edge.
 type Edge
-    tail::Union(Interface, Nothing)
-    head::Union(Interface, Nothing)
+    tail::Interface
+    head::Interface
 
     function Edge(tail::Interface, head::Interface)
-        if  typeof(tail) == Nothing ||
-            typeof(head) == Nothing ||
+        if  typeof(head.message) == Nothing ||
+            typeof(tail.message) == Nothing ||
             typeof(head.message) == typeof(tail.message)
-
             tail.partner = head
             head.partner = tail
             new(tail, head)
@@ -66,25 +88,74 @@ end
 # calculatemessage calculates the outgoing message on the sink interface (half-edge).
 # Every combination of node and message types will get an individual signature for the function call.
 #
-function calculatemessage(  sink::Interface,
+function calculatemessage(
+                            interface::Interface,
+                            node::ConstantNode,
+                            inboundMessageTypes,
+                            messageType::DataType)
+    print("Message from constant node")
+    interface.message = ScalarParameterMessage(node.constant)
+end
+
+function calculatemessage{T<:Union(GaussianMessage,ScalarParameterMessage)}(
+                            interface::Interface,
                             node::MultiplicationNode,
-                            multiplierMessage::ScalarParameterMessage,
-                            sourceMessage::GaussianMessage)
+                            inboundMessageTypes::T,
+                            messageType::DataType)
     # Multiplication of a Gaussian message
-    sink_V = multiplierMessage.value' * sourceMessage.V * multiplierMessage.value
-    sink_m = multiplierMessage.value * sourceMessage.m
-    sink.message = GaussianMessage(sink_V, sink_m)
+    #sink_V = multiplierMessage.value' * sourceMessage.V * multiplierMessage.value
+    #sink_m = multiplierMessage.value * sourceMessage.m
+    print("Message from multiplier node")
+    interface.message = GaussianMessage()
 end
 
-# call for calculatemessage that calculates the incoming messages by recursion.
-function calculatemessage(  sink::Interface,
-                            node::MultiplicationNode)
-    # TODO: Collect all incoming messages
-    calculatemessage(sink, node, ScalarParameterMessage(4.0), GaussianMessage())
+# Calculate the outbound message (of type messageType) on a specific interface of a specified node
+function calculatemessage(interface::Interface, node::Node, messageType::DataType=Message)
+    # Sanity check
+    if !is(interface.node, node)
+        error("Specified interface does not belong to the specified node")
+    end
+
+    # Collect all inbound messages
+    inbound_message_types = Union() # Union of all inbound message types
+    node_interfaces = interfaces(node) # TODO: check if there is a better way to iterate over the interfaces
+    for node_interface in node_interfaces
+        if is(node_interface, interface) continue end
+        if node_interface.partner == nothing
+            error("Cannot receive messages on disconnected interface")
+        end
+        if node_interface.partner.message == nothing
+            # Recursive call to calculate required inbound message
+            calculatemessage(node_interface.partner)
+            if node_interface.partner.message == nothing
+                error("Could not calculate required inbound message")
+            end
+            inbound_message_types = Union(inbound_message_types, typeof(node_interface.partner.message))
+        end
+    end
+
+    # Calculate the actual message
+    print(typeof(inbound_message_types))
+    calculatemessage(interface, node, inbound_message_types, messageType)
+
+    # Clear all inbound messages
+    for node_interface in node_interfaces
+        if is(node_interface, interface) continue end
+        node_interface.partner.message = nothing
+    end
 end
 
-calculatemessage(sink::Interface) = calculatemessage(sink, sink.node)
+# Calculate the outbound message on a specified interface
+calculatemessage(interface::Interface) = calculatemessage(interface, interface.node)
 
+# Calculate the outbound messages on all interfaces of a specific node
+function calculatemessage(node::Node)
+    for interface in interfaces(node)
+        calculatemessage(interface, node)
+    end
+end
+
+# Calculate forward/backward messages on edge
 calculateforwardmessage(edge::Edge) = calculatemessage(edge.tail)
 calculatebackwardmessage(edge::Edge) = calculatemessage(edge.head)
 
