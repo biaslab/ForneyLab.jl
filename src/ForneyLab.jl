793acfd43967dc394ecdcad6d38b466d0c30579c
Marco Cox <mail@marcocox.com>
2014-04-15 18:04:41 +0200

    Code refactoring and multiple fixes. Be aware that this code is still *very* dirty and experimental. Most important change: interfaces of a node are now stored in an array for easy iteration and identification. An interface has a unique id, and named references are just pointers.
    TODO: Update the tests, move stuff to separate files, define exports.
    
    Example testing code:
    
    include("ForneyLab.jl/src/ForneyLab.jl")
    c1 = ForneyLab.ConstantNode(ForneyLab.ScalarParameterMessage(3.0))
    c2 = ForneyLab.ConstantNode(ForneyLab.ScalarParameterMessage(2.0))
    x = ForneyLab.MultiplicationNode()
    e1 = ForneyLab.Edge(c1.interface, x.source)
    e2 = ForneyLab.Edge(c2.interface, x.multiplier)
    ForneyLab.calculatemessage(x.sink)

--- src/ForneyLab.jl
+++ src/ForneyLab.jl
@@ -1,27 +1,14 @@
 module ForneyLab
 
-#
-# Messages
-#
+#############################
+#   Top-level types
+#############################
+
 abstract Message
-
-type GaussianMessage <: Message
-    V::Array{Float64}
-    m::Array{Float64}
-end
-GaussianMessage() = GaussianMessage([1.0], [0.0])
-
-type ScalarParameterMessage{T} <: Message
-    value::T
-end
-ScalarParameterMessage() = ScalarParameterMessage(1.0)
-
-#
-# Nodes
-#
 abstract Node
 
-# An interface is a half-edge that connects to another interface to form a complete edge.
+# Interface: belongs to a node, can be linked to a partner interface and can hold a message.
+# It can be viewed as a half-edge that connects to a partner interface to form a complete edge.
 type Interface
     node::Node
     partner::Union(Interface, Nothing)
@@ -29,44 +16,9 @@ type Interface
 end
 Interface(node::Node) = Interface(node, nothing, nothing)
 
-# interfaces(node) returns an array of interfaces for iterating
-function interfaces(node::Node)
-    node_interfaces = Array(Interface,0)
-    for field in names(node)
-        if typeof(getfield(node, field)) == Interface
-            node_interfaces = [node_interfaces, getfield(node, field)]
-        end
-    end
-
-    return node_interfaces
-end
-
-# Constant node
-type ConstantNode <: Node
-    constant::Float64
-    interface::Interface
-    function ConstantNode(constant)
-        self = new(constant)
-        self.interface = Interface(self)
-        return self
-    end
-end
-
-# Mulitiply source with parameter multiplier: sink = multiplier * source
-type MultiplicationNode <: Node
-    multiplier::Interface
-    source::Interface
-    sink::Interface
-    function MultiplicationNode()
-        self = new()
-        self.multiplier = Interface(self)
-        self.source = Interface(self)
-        self.sink = Interface(self)
-        return self
-    end
-end
-
-# Combine two interfaces to form a complete edge.
+# Edge: combines two interfaces.
+# Mostly useful for code readability, not used internally.
+# Forward messages are defined to flow from tail to head
 type Edge
     tail::Interface
     head::Interface
@@ -84,30 +36,69 @@ type Edge
     end
 end
 
-#
-# calculatemessage calculates the outgoing message on the sink interface (half-edge).
-# Every combination of node and message types will get an individual signature for the function call.
-#
-function calculatemessage(
-                            interface::Interface,
-                            node::ConstantNode,
-                            inboundMessageTypes,
-                            messageType::DataType)
-    print("Message from constant node")
-    interface.message = ScalarParameterMessage(node.constant)
+#############################
+# Messages
+#############################
+
+# GaussianMessage: encodes a Gaussian PDF
+type GaussianMessage <: Message
+    V::Array{Float64}
+    m::Array{Float64}
+end
+GaussianMessage() = GaussianMessage([1.0], [0.0])
+
+# ScalarParameterMessage: simply holds a scalar value
+type ScalarParameterMessage{T} <: Message
+    value::T
+end
+ScalarParameterMessage() = ScalarParameterMessage(1.0)
+
+#############################
+# Nodes
+#############################
+
+# ConstantNode: has just one interface and always sends out a constant message
+type ConstantNode <: Node
+    constant::Message
+    interfaces::Array{Interface,1}
+    interface::Interface
+
+    function ConstantNode(constant::Message)
+        self = new(constant, Array(Interface, 1))
+        # Create interface
+        self.interfaces[1] = Interface(self)
+        # Init named interface handle
+        self.interface = self.interfaces[1]
+        return self
+    end
 end
 
-function calculatemessage{T<:Union(GaussianMessage,ScalarParameterMessage)}(
-                            interface::Interface,
-                            node::MultiplicationNode,
-                            inboundMessageTypes::T,
-                            messageType::DataType)
-    # Multiplication of a Gaussian message
-    #sink_V = multiplierMessage.value' * sourceMessage.V * multiplierMessage.value
-    #sink_m = multiplierMessage.value * sourceMessage.m
-    print("Message from multiplier node")
-    interface.message = GaussianMessage()
+# Mulitiplication node: multiply source with parameter multiplier:
+# sink = multiplier * source
+type MultiplicationNode <: Node
+    interfaces::Array{Interface,1}
+    multiplier::Interface
+    source::Interface
+    sink::Interface
+
+    function MultiplicationNode()
+        self = new(Array(Interface, 3))
+        # Create interfaces
+        self.interfaces[1] = Interface(self)
+        self.interfaces[2] = Interface(self)
+        self.interfaces[3] = Interface(self)
+        # Init named interface handles
+        self.source     = self.interfaces[1]
+        self.multiplier = self.interfaces[2]
+        self.sink       = self.interfaces[3]
+        return self
+    end
 end
+
+
+#############################
+# Generic methods
+#############################
 
 # Calculate the outbound message (of type messageType) on a specific interface of a specified node
 function calculatemessage(interface::Interface, node::Node, messageType::DataType=Message)
@@ -116,10 +107,9 @@ function calculatemessage(interface::Interface, node::Node, messageType::DataTyp
         error("Specified interface does not belong to the specified node")
     end
 
-    # Collect all inbound messages
+    # Calculate all inbound messages
     inbound_message_types = Union() # Union of all inbound message types
-    node_interfaces = interfaces(node) # TODO: check if there is a better way to iterate over the interfaces
-    for node_interface in node_interfaces
+    for node_interface in node.interfaces
         if is(node_interface, interface) continue end
         if node_interface.partner == nothing
             error("Cannot receive messages on disconnected interface")
@@ -134,12 +124,23 @@ function calculatemessage(interface::Interface, node::Node, messageType::DataTyp
         end
     end
 
+    # Collect all inbound messages
+    inbound_messages = Array(inbound_message_types, length(node.interfaces))
+    interface_id = 0
+    for node_interface_id = 1:length(node.interfaces)
+        node_interface = node.interfaces[node_interface_id]
+        if is(node_interface, interface)
+            interface_id = node_interface_id
+            continue
+        end
+        inbound_messages[node_interface_id] = node.interfaces[node_interface_id].partner.message
+    end
+
     # Calculate the actual message
-    print(typeof(inbound_message_types))
-    calculatemessage(interface, node, inbound_message_types, messageType)
+    calculatemessage(interface_id, node, inbound_messages, messageType)
 
     # Clear all inbound messages
-    for node_interface in node_interfaces
+    for node_interface in node.interfaces
         if is(node_interface, interface) continue end
         node_interface.partner.message = nothing
     end
@@ -150,13 +151,41 @@ calculatemessage(interface::Interface) = calculatemessage(interface, interface.n
 
 # Calculate the outbound messages on all interfaces of a specific node
 function calculatemessage(node::Node)
-    for interface in interfaces(node)
+    for interface in node.interfaces
         calculatemessage(interface, node)
     end
 end
 
-# Calculate forward/backward messages on edge
+# Calculate forward/backward messages on an edge
 calculateforwardmessage(edge::Edge) = calculatemessage(edge.tail)
 calculatebackwardmessage(edge::Edge) = calculatemessage(edge.head)
 
+#############################
+# Node/message specific methods
+#############################
+#
+# calculatemessage calculates the outgoing message on the sink interface (half-edge).
+# Every combination of node and message types will get an individual signature for the function call.
+#
+
+function calculatemessage{T<:Message}(
+                            interfaceId::Int,
+                            node::ConstantNode,
+                            inboundMessages::Array{T,1},
+                            messageType::DataType)
+    node.interfaces[interfaceId].message = node.constant
+end
+
+function calculatemessage{T<:Union(GaussianMessage,ScalarParameterMessage)}(
+                            interfaceId::Int,
+                            node::MultiplicationNode,
+                            inboundMessages::Array{T,1},
+                            messageType::DataType)
+    # Multiplication for Gaussian message
+    #sink_V = multiplierMessage.value' * sourceMessage.V * multiplierMessage.value
+    #sink_m = multiplierMessage.value * sourceMessage.m
+    node.interfaces[interfaceId].message = GaussianMessage()
+end
+
+# TODO: move all node/message specific definitions and methods to separate files
 end # module ForneyLab
