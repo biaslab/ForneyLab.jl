ea9f453950276678103c8831f9efb9e916404056
Thijs van de Laar <thijsvdlaar@hotmail.com>
2014-04-22 16:57:05 +0200

    refactored calculatemessage! function on matrix multiplication node

--- src/nodes/matrixmultiplication.jl
+++ src/nodes/matrixmultiplication.jl
@@ -43,6 +43,21 @@ type MatrixMultiplicationNode <: Node
 end
 MatrixMultiplicationNode(; args...) = MatrixMultiplicationNode([1.0]; args...)
 
+# Rule set for backward propagation
+backward_m_rule(A::Array{Number,2}, m::Array{Number,1}) = inv(A) * m
+backward_m_rule(A::Array{Number,2}, m::Array{Number,1}, W::Array{Number,2}) = pinv(A' * W * A) * A' * W * m
+backward_V_rule(A::Array{Number,2}, V::Array{Number,2}) = inv(A) * V * inv(A)' # TODO: inversion only once
+backward_W_rule(A::Array{Number,2}, W::Array{Number,2}) = A' * W * A
+backward_xi_rule(A::Array{Number,2}, xi::Array{Number,1}) = A' * xi
+
+# Rule set for forward propagation
+forward_m_rule(A::Array{Number,2}, m::Array{Number,1}) = A * m
+forward_V_rule(A::Array{Number,2}, V::Array{Number,2}) = A * V * A'
+forward_W_rule(A::Array{Number,2}, W::Array{Number,2}) = inv(A)' * W * inv(A) # TODO: inversion only once
+forward_xi_rule(A::Array{Number,2}, xi::Array{Number,1}) = inv(A)' * xi
+forward_xi_rule(A::Array{Number,2}, xi::Array{Number,1}, V::Array{Number,2}) = pinv(A * V * A') * A * V * xi # Combination of xi and V
+
+# Calculations for a gaussian message type; Korl (2005), table 4.1
 function calculatemessage!( outbound_interface_id::Int,
                             node::MatrixMultiplicationNode,
                             inbound_messages::Array{GaussianMessage,1})
@@ -50,73 +65,75 @@ function calculatemessage!( outbound_interface_id::Int,
         # Backward message
         msg_in = inbound_messages[2]
         msg = deepcopy(msg_in)
-        if typeof(msg.xi)==Array
-            msg.xi = transpose(node.A) * msg_in.xi
-            msg.m = [] # Invalidate m, no need to pre-calculate
-        end
-        if typeof(msg.m)==Array || is(msg.W, nothing)
-            if isposdef(node.A) && isposdef(msg_in.W)
-                msg.m = inv(node.A) * msg_in.m
-            else
-                msg.m = pinv(transpose(node.A) * msg_in.W * node.A) * transpose(node.A) * msg_in.W * msg_in.m
-            end
-        end
-        if typeof(msg.W)==Array
-            msg.W = transpose(node.A) * msg_in.W * node.A
-            msg.V = [] # Invalidate V, no need to pre-calculate
-        end
-        if typeof(msg.V)==Array
-            # TODO: validate that A is non-singular
-            A_inv = inv(node.A)
-            msg.V = A_inv * msg_in.V * transpose(A_inv)
+
+        # Select parameterization
+        # Order is from least to most computationally intensive
+        if msg.xi != nothing && msg.W != nothing
+            msg.m = nothing
+            msg.V = nothing
+            msg.W = backward_W_rule(node.A, msg.W)
+            msg.xi = backward_xi_rule(node.A, msg.xi)
+        elseif msg.m != nothing && msg.W != nothing
+            msg.m = backward_m_rule(node.A, msg.m, msg.W) # Long version of the rule
+            msg.V = nothing
+            msg.W = backward_W_rule(node.A, msg.W)
+            msg.xi = nothing
+        elseif msg.m != nothing && msg.V != nothing && isposdef(node.A) # TODO: How to check for positive definiteness of W?
+            msg.m = backward_m_rule(node.A, msg.m) # Short version of the rule, only valid if A and W are positive definite
+            msg.V = backward_V_rule(node.A, msg.V)
+            msg.W = nothing
+            msg.xi = nothing
+        else
+            error("Insufficient input to calculate outbound message on interface ", outbound_interface_id, " of ", typeof(node), " ", node.name)
         end
     elseif outbound_interface_id == 2
         # Forward message
         msg_in = inbound_messages[1]
         msg = deepcopy(msg_in)
-        if typeof(msg.m)==Array
-            msg.m = node.A * msg_in.m
-            msg.xi = nothing # Invalidate xi, no need to pre-calculate
-        end
-        if typeof(msg.xi)==Array
-            if isposdef(node.A) && isposdef(msg_in.V)
-                msg.xi = transpose(inv(node.A)) * msg_in.xi
-            else
-                msg.xi = pinv(node.A * msg_in.V * transpose(node.A)) * node.A * msg_in.V * msg_in.xi
-            end
-        end
-        if typeof(msg.V)==Array
-            msg.V = node.A * msg_in.V * transpose(node.A)
-            msg.W = nothing # Invalidate W
-        end
-        if typeof(msg.W)==Array || typeof(msg.xi)==Array
-            # Calculate W if V is unknown or xi is used
-            # TODO: check if A is indeed non-singular and msg_in.W is available
-            # TODO: otherwise, check if we can use W = inv(V) instead
-            A_inv = inv(node.A)
-            msg.W = transpose(A_inv) * msg_in.W * A_inv
+
+        # Select parameterization
+        # Order is from least to most computationally intensive
+        if msg.m != nothing && msg.V != nothing
+            msg.m = forward_m_rule(node.A, msg.m)
+            msg.V = forward_V_rule(node.A, msg.V)
+            msg.W = nothing
+            msg.xi = nothing
+        elseif msg.m != nothing && msg.W != nothing
+            msg.m = forward_m_rule(node.A, msg.m)
+            msg.V = nothing
+            msg.W = forward_W_rule(node.A, msg.W)
+            msg.xi = nothing
+        elseif msg.xi != nothing && msg.W != nothing && isposdef(node.A) # TODO: How to check for positive definiteness of V?
+            msg.m = nothing
+            msg.V = nothing
+            msg.W = forward_W_rule(node.A, msg.W)
+            msg.xi = forward_xi_rule(node.A, msg.xi) # Short version of the rule, only valid if A and V are positive definite
+        elseif msg.xi != nothing && msg.V != nothing && msg.W != nothing
+            msg.m = nothing
+            msg.V = nothing
+            msg.W = forward_W_rule(node.A, msg.W)
+            msg.xi = forward_xi_rule(node.A, msg.xi, msg.V) # Long version of the rule
+        else
+            error("Insufficient input to calculate outbound message on interface ", outbound_interface_id, " of ", typeof(node), " ", node.name)
         end
     end
 
+    # Set the outbound message
     node.interfaces[outbound_interface_id].message = msg
 end
 
+# Calculations for a general message type
 function calculatemessage!( outbound_interface_id::Int,
                             node::MatrixMultiplicationNode,
-                            inbound_messages::Array{GaussianMessage,1})
+                            inbound_messages::Array{GeneralMessage,1})
     if outbound_interface_id == 1
         # Backward message
-        msg_in = inbound_messages[2].value
-        msg = deepcopy(msg_in)
-        if isposdef(node.A)
-            msg.value = inv(node.A) * msg_in.value
-        else
-            msg.value = pinv(node.A) * msg_in.value
-        end
+        msg = GeneralMessage( pinv(node.A) * inbound_messages[2].value )
     elseif outbound_interface_id == 2
         # Forward message
-        msg = GeneralMessage(node.A * inbound_messages[1].value)
+        msg = GeneralMessage( node.A * inbound_messages[1].value )
     end
 
+    # Set the outbound message
     node.interfaces[outbound_interface_id].message = msg
 end
