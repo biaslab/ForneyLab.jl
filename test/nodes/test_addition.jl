e6527fdabd923c8dc938d8d1ac27f2135d5ef348
Thijs van de Laar <thijsvdlaar@hotmail.com>
2014-04-28 14:45:05 +0200

    tests for addition node pass

--- test/nodes/test_addition.jl
+++ test/nodes/test_addition.jl
@@ -74,14 +74,71 @@ facts("AdditionNode") do
         end
     end
 
+    # Tests on Gaussian messages use the update rules from Korl (2005), 
+    # "A Factor Graph Approach to Signal Modelling, System Identification and Filtering.", Table 4.1.
     context("AdditionNode should propagate a multivariate GaussianMessage") do
+        node = AdditionNode()
         context("Multivariate GaussianMessage with (m,V) parametrization") do
+            mean = [1.0:3.0]
+            variance = reshape([4.0, 3.0, 2.0,
+                                3.0, 4.0, 3.0,
+                                2.0, 3.0, 4.0], 3, 3)
+            inbound_msg_1 = GaussianMessage(m=mean, V=variance)
+            inbound_msg_2 = GaussianMessage(m=mean, V=variance)
+            # Forward message
+            msg = calculateMessage!(3, node, [inbound_msg_1, inbound_msg_2])
+            @fact node.interfaces[3].message => msg
+            @fact node.interfaces[3].message.m => [2.0, 4.0, 6.0]
+            @fact node.interfaces[3].message.V => 2.0*variance
+            # Backward messages
+            for input = [1,2]
+                msg = calculateMessage!(input, node, [inbound_msg_1, inbound_msg_2])
+                @fact node.interfaces[input].message => msg
+                @fact node.interfaces[input].message.m => [0.0, 0.0, 0.0]
+                @fact node.interfaces[input].message.V => 2.0*variance
+            end
         end
 
         context("Multivariate GaussianMessage with (m,W) parametrization") do
+            mean = [1.0:3.0]
+            precision = reshape([4.0, 3.0, 2.0,
+                                 3.0, 4.0, 3.0,
+                                 2.0, 3.0, 4.0], 3, 3)
+            inbound_msg_1 = GaussianMessage(m=mean, W=precision)
+            inbound_msg_2 = GaussianMessage(m=mean, W=precision)
+            # Forward message
+            msg = calculateMessage!(3, node, [inbound_msg_1, inbound_msg_2])
+            @fact node.interfaces[3].message => msg
+            @fact node.interfaces[3].message.m => [2.0, 4.0, 6.0]
+            @fact maximum(abs(node.interfaces[3].message.W - reshape([2.0, 1.5, 1.0, 1.5, 2.0, 1.5, 1.0, 1.5, 2.0], 3, 3))) < epsilon => true
+            # Backward messages
+            for input = [1,2]
+                msg = calculateMessage!(input, node, [inbound_msg_1, inbound_msg_2])
+                @fact node.interfaces[input].message => msg
+                @fact node.interfaces[input].message.m => [0.0, 0.0, 0.0]
+                @fact maximum(abs(node.interfaces[3].message.W - reshape([2.0, 1.5, 1.0, 1.5, 2.0, 1.5, 1.0, 1.5, 2.0], 3, 3))) < epsilon => true
+            end
         end
 
         context("Multivariate GaussianMessage with (xi,V) parametrization") do
+            xi = [1.0:3.0]
+            variance = reshape([4.0, 3.0, 2.0,
+                                3.0, 4.0, 3.0,
+                                2.0, 3.0, 4.0], 3, 3)
+            inbound_msg_1 = GaussianMessage(xi=xi, V=variance)
+            inbound_msg_2 = GaussianMessage(xi=xi, V=variance)
+            # Forward message
+            msg = calculateMessage!(3, node, [inbound_msg_1, inbound_msg_2])
+            @fact node.interfaces[3].message => msg
+            @fact maximum(abs(node.interfaces[3].message.xi - [1.0, 2.0, 3.0])) < epsilon => true
+            @fact node.interfaces[3].message.V => 2.0*variance
+            # Backward messages
+            for input = [1,2]
+                msg = calculateMessage!(input, node, [inbound_msg_1, inbound_msg_2])
+                @fact node.interfaces[input].message => msg
+                @fact node.interfaces[input].message.xi => [0.0, 0.0, 0.0]
+                @fact node.interfaces[input].message.V => 2.0*variance
+            end
         end
     end
 
