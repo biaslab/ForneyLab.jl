8594f5d7bc58fa6e018e86f03ebee1461dae617f
Thijs van de Laar <thijsvdlaar@hotmail.com>
2014-05-01 11:24:54 +0200

    composite node constructor

--- src/nodes/composite/gain_equality.jl
+++ src/nodes/composite/gain_equality.jl
@@ -1,1 +1,79 @@
-# Definition for a composite gain-equality node
+############################################
+# GainEqualityCompositeNode
+############################################
+# Description:
+#   Composite gain-equality node.
+#
+#    in1      out1
+#   ----->[=]------>
+#          |
+#         [A]
+#          | out2
+#          v
+#
+#   in1 = out1 = A*out2
+#   Example:
+#       GainEqualityCompositeNode([1.0]; name="my_node")
+#   Gain A is fixed and defined through the constructor.
+#
+# Interface ids, (names) and supported message types:
+#   1. in1:
+#       GaussianMessage
+#   2. out1:
+#       GaussianMessage
+#   3. out2:
+#       GaussianMessage
+#
+############################################
+
+export GainEqualityCompositeNode
+
+type GainEqualityCompositeNode <: Node
+    # Basic node properties.
+    # Use_composite_update_rule is a flag for indicating use of shortcut update rules. When set to true, the internals are ignored and the updated
+    # outgoing message is calculated via the provided update rules. When set to false, messages are passed through the internals of the composite node.
+    A::Array
+    #use_composite_update_rules::Bool
+    name::ASCIIString
+    interfaces::Array{Interface,1}
+    # Pointers to internal nodes
+    equality_node::EqualityNode
+    fixed_gain_node::FixedGainNode
+    # Helper fields filled by constructor
+    in1::Interface
+    out1::Interface
+    out2::Interface
+    A_inv::Array{Float64, 2} # holds pre-computed inv(A) if possible
+
+    function GainEqualityCompositeNode(A::Array; args...)
+        name = "#undef"
+        for (key, val) in args
+            if key==:name
+                name=val
+            end
+        end
+
+        # Deepcopy A to avoid an unexpected change of the input argument A. Ensure that A is a matrix.
+        A_copy = ensureMatrix(deepcopy(A))
+        # Build composite node
+        self = new(A_copy, name, Array(Interface, 3))
+        # Define the internals of the composite node
+        self.equality_node = EqualityNode()
+        self.fixed_gain_node = FixedGainNode(A_copy)
+        Edge(self.equality_node.interfaces[2], self.fixed_gain_node.in1) # Internal edge
+
+        # Set pointers of the composite node interfaces to the internal nodes
+        self.interfaces[1] = self.equality_node.interfaces[1]
+        self.interfaces[2] = self.fixed_gain_node.out
+        self.interfaces[3] = self.equality_node.interfaces[3]
+        # Init named interface handles
+        self.in1 = self.interfaces[1]
+        self.out1 = self.interfaces[2]
+        self.out2 = self.interfaces[3]
+        # Pointer to precomputed inv(A), needed for quick calculat rules
+        self.A_inv = self.fixed_gain_node.A_inv
+        return self
+    end
+end
+GainEqualityCompositeNode(; args...) = GainEqualityCompositeNode([1.0]; args...)
+
--- test/nodes/composite/test_gain_equality.jl
+++ test/nodes/composite/test_gain_equality.jl
@@ -1,1 +1,25 @@
-# Tests for a composite gain-equality node
+facts("GainEqualityCompositeNode") do
+    context("GainEqualityCompositeNode() should initialize a GainEqualityCompositeNode with 3 interfaces") do
+        node = GainEqualityCompositeNode()
+        @fact typeof(node) => GainEqualityCompositeNode
+        @fact length(node.interfaces) => 3
+        @fact node.in1 => node.interfaces[1]
+        @fact node.out1 => node.interfaces[2]
+        @fact node.out2 => node.interfaces[3]
+        @fact typeof(node.A) => Array{Float64, 2} # cast single value to matrix
+    end
+
+    context("GainEqualityCompositeNode() should define an internal Equality and FixedGain node") do
+        node = GainEqualityCompositeNode([5.0])
+        @fact typeof(node.equality_node) => EqualityNode
+        @fact typeof(node.fixed_gain_node) => FixedGainNode
+        @fact node.fixed_gain_node.A => reshape([5.0],1,1)
+    end
+
+    context("GainEqualityCompositeNode() should point its own interfaces to the internal node interfaces") do
+        node = GainEqualityCompositeNode([1.0])
+        @fact node.interfaces[1] => node.equality_node.interfaces[1]
+        @fact node.interfaces[2] => node.fixed_gain_node.interfaces[2]
+        @fact node.interfaces[3] => node.equality_node.interfaces[3]
+    end
+end
