fbbf58e7041d7e782767a4e5b0d3502d1b3c2183
Marco Cox <mail@marcocox.com>
2014-05-13 15:12:34 +0200

    Moved helper functions to helpers.jl. Implemented getArgumentValue() function.

--- src/ForneyLab.jl
+++ src/ForneyLab.jl
@@ -1,33 +1,20 @@
 module ForneyLab
 
 export Message, Node, Interface, Edge
-# User functions
 export calculateMessage!, calculateMessages!, calculateForwardMessage!, calculateBackwardMessage!, calculateMarginal, clearMessages!
-# Helper functions
-export isApproxEqual
 
-#############################
-# Helper methods
-#############################
+# Verbosity
 verbose = false
-
-# ensureMatrix: ensure that the input is a 2D array or nothing
-ensureMatrix{T<:Number}(arr::Array{T, 2}) = arr
-ensureMatrix{T<:Number}(arr::Array{T, 1}) = reshape(arr, 1, 1)
-ensureMatrix(n::Nothing) = nothing
-
-# isRoundedPosDef: is input matrix positive definite? Round to prevent fp precision problems that isposdef() suffers from.
-isRoundedPosDef{T<:FloatingPoint}(arr::Array{T, 2}) = ishermitian(round(arr, 12)) && isposdef(arr, 'L')
-
-# Helper function to check approximate equality
-isApproxEqual(arg1, arg2) = maximum(abs(arg1-arg2)) < 1.0e-12
-
-# verbosity helpers
 setVerbose(verbose_mode=true) = global verbose=verbose_mode
 printVerbose(msg) = if verbose println(msg) end
 
+# Helpers
+include("helpers.jl")
+
+# Other includes
 import Base.show
 
+# Top-level abstracts
 abstract Message
 
 abstract Node
--- test/test_forneylab.jl
+++ test/test_forneylab.jl
@@ -7,13 +7,7 @@ module TestForneyLab
 using FactCheck
 using ForneyLab
 
-facts("Helper functions") do
-    context("ensureMatrix should convert an array with one element to a matrix type") do
-        @fact typeof(ForneyLab.ensureMatrix([1.0])) => Array{Float64, 2} # Cast 1D to 2D array
-        @fact ForneyLab.ensureMatrix([1.0]) => reshape([1.0], 1, 1)
-        @fact ForneyLab.ensureMatrix(eye(2)) => eye(2)
-    end
-end
+include("test_helpers.jl")
 
 facts("General node properties") do
     context("Node properties should include interfaces and name") do
