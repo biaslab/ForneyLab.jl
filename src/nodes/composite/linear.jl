9f85dd552a46849e64dcc0074c0c9eed5e0b27ed
Thijs <thijsvdlaar@hotmail.com>
2014-08-05 11:34:51 +0200

    rewritten linear node file

--- src/nodes/composite/linear.jl
+++ src/nodes/composite/linear.jl
@@ -77,99 +77,39 @@ type LinearCompositeNode <: CompositeNode
 end
 
 
-
-
-
-
-
-
-
-
-
-
-
-
 ############################################
 # Variational update functions
 ############################################
 
-function updateNodeMessage!(outbound_interface_id::Int,
-                            node::LinearCompositeNode,
-                            inbound_messages_value_types::Type{Union(GaussianDistribution, InverseGammaDistribution)},
-                            outbound_message_value_type::Type{GaussianDistribution})
+function updateNodeMessage!(node::LinearCompositeNode,
+                            outbound_interface_id::Int,
+                            outbound_message_value_type::Type{GaussianDistribution},
+                            marg_in1::Any,
+                            marg_slope::Any,
+                            marg_offset::Any,
+                            marg_noise::GammaDistribution,
+                            marg_out::Any)
     # Variational update function, takes the marginals as input.
+    # Sends to any interface carrying a Gaussian message, while using precision parameterized noise
     # Derivation for the update rule can be found in the derivations notebook.
 
     dist_out = getOrCreateMessage(node.interfaces[outbound_interface_id], outbound_message_value_type).value
 
     # Ensure right parameterization
-    for i = [1, 2, 3, 5] # just get all of them, all marginals need to be defined anyway
-        ensureMVParametrization!(node.interfaces[i].edge.marginal)
+    for param = [marg_in1, marg_slope, marg_offset, marg_out] # just get all of them, all marginals need to be defined anyway
+        ensureMWParametrization!(param)
     end
     # Get the variables beforehand for more readable update equations
-    mu_a = node.slope.edge.marginal.m[1]
-    s_a = node.slope.edge.marginal.V[1, 1] # sigma_a^2
-    mu_b = node.offset.edge.marginal.m[1]
-    s_b = node.offset.edge.marginal.V[1, 1] # sigma_b^2
-    mu_x2 = node.out.edge.marginal.m[1]
-    s_x2 = node.out.edge.marginal.V[1, 1] # sigma_x2^2
-    mu_x1 = node.in1.edge.marginal.m[1]
-    s_x1 = node.in1.edge.marginal.V[1, 1] # sigma_x1^2
-    a_s = node.noise.edge.marginal.a
-    b_s = node.noise.edge.marginal.b
-
-    if outbound_interface_id == 1 # in1
-        dist_out.m = [(mu_a*(mu_x2 - mu_b))/(s_a + mu_a^2)]
-        dist_out.V = reshape([(a_s + 1)/(b_s*(mu_a^2 + s_a))], 1, 1)
-        dist_out.W = nothing
-        dist_out.xi = nothing
-    elseif outbound_interface_id == 2 # a
-        dist_out.m = [(mu_x1*(mu_x2 - mu_b))/(s_x1 + mu_x1^2)]
-        dist_out.V = reshape([(a_s + 1)/(b_s*(mu_x1^2 + s_x1))], 1, 1)
-        dist_out.W = nothing
-        dist_out.xi = nothing
-    elseif outbound_interface_id == 3 # b
-        dist_out.m = [mu_x2 - mu_a*mu_x1]
-        dist_out.V = reshape([(a_s + 1)/b_s], 1, 1)
-        dist_out.W = nothing
-        dist_out.xi = nothing
-    elseif outbound_interface_id == 5 # out
-        dist_out.m = [mu_a*mu_x1 + mu_b]
-        dist_out.V = reshape([(a_s + 1)/b_s], 1, 1)
-        dist_out.W = nothing
-        dist_out.xi = nothing
-    else
-        error("Invalid outbound interface id $(outbound_interface_id), on $(typeof(node)) $(node.name).")
-    end
-
-    # Set the outbound message
-    return node.interfaces[outbound_interface_id].message
-end
-
-function updateNodeMessage!(outbound_interface_id::Int,
-                            node::LinearCompositeNode,
-                            inbound_messages_value_types::Type{Union(GaussianDistribution, GammaDistribution)},
-                            outbound_message_value_type::Type{GaussianDistribution})
-    # Variational update function, takes the marginals as input.
-    # Derivation for the update rule can be found in the derivations notebook.
-
-    dist_out = getOrCreateMessage(node.interfaces[outbound_interface_id], outbound_message_value_type).value
-
-    # Ensure right parameterization
-    for i = [1, 2, 3, 5] # just get all of them, all marginals need to be defined anyway
-        ensureMWParametrization!(node.interfaces[i].edge.marginal)
-    end
-    # Get the variables beforehand for more readable update equations
-    mu_a = node.slope.edge.marginal.m[1]
-    gam_a = node.slope.edge.marginal.W[1, 1] # gamma_a
-    mu_b = node.offset.edge.marginal.m[1]
-    gam_b = node.offset.edge.marginal.W[1, 1] # gamma_b
-    mu_x2 = node.out.edge.marginal.m[1]
-    gam_x2 = node.out.edge.marginal.W[1, 1] # gamma_x2
-    mu_x1 = node.in1.edge.marginal.m[1]
-    gam_x1 = node.in1.edge.marginal.W[1, 1] # gamma_x1
-    a_gam = node.noise.edge.marginal.a
-    b_gam = node.noise.edge.marginal.b
+    mu_a = marg_slope.m[1]
+    gam_a = marg_slope.W[1, 1] # gamma_a
+    mu_b = marg_offset.m[1]
+    gam_b = marg_offset.W[1, 1] # gamma_b
+    mu_x2 = marg_out.m[1]
+    gam_x2 = marg_out.W[1, 1] # gamma_x2
+    mu_x1 = marg_in1.m[1]
+    gam_x1 = marg_in1.W[1, 1] # gamma_x1
+    a_gam = marg_noise.a
+    b_gam = marg_noise.b
 
     if outbound_interface_id == 1 # in1
         dist_out.m = [(mu_a*(mu_x2 - mu_b))/(inv(gam_a) + mu_a^2)]
@@ -199,33 +139,56 @@ function updateNodeMessage!(outbound_interface_id::Int,
     return node.interfaces[outbound_interface_id].message
 end
 
-function updateNodeMessage!(outbound_interface_id::Int,
-                            node::LinearCompositeNode,
-                            inbound_messages_value_types::Type{GaussianDistribution},
-                            outbound_message_value_type::Type{InverseGammaDistribution})
+function updateNodeMessage!(node::LinearCompositeNode,
+                            outbound_interface_id::Int,
+                            outbound_message_value_type::Type{GaussianDistribution},
+                            marg_in1::Any,
+                            marg_slope::Any,
+                            marg_offset::Any,
+                            marg_noise::InverseGammaDistribution,
+                            marg_out::Any)
     # Variational update function, takes the marginals as input.
+    # Sends to any interface carrying a Gaussian message, while using variance parameterized noise
     # Derivation for the update rule can be found in the derivations notebook.
 
     dist_out = getOrCreateMessage(node.interfaces[outbound_interface_id], outbound_message_value_type).value
 
     # Ensure right parameterization
-    for i = [1, 2, 3, 5] # just get all of them, all marginals need to be defined anyway
-        ensureMVParametrization!(node.interfaces[i].edge.marginal)
+    for param = [marg_in1, marg_slope, marg_offset, marg_out] # just get all of them, all marginals need to be defined anyway
+        ensureMVParametrization!(param)
     end
-
     # Get the variables beforehand for more readable update equations
-    mu_a = node.slope.edge.marginal.m[1]
-    s_a = node.slope.edge.marginal.V[1, 1] # sigma_a^2
-    mu_b = node.offset.edge.marginal.m[1]
-    s_b = node.offset.edge.marginal.V[1, 1] # sigma_b^2
-    mu_x2 = node.out.edge.marginal.m[1]
-    s_x2 = node.out.edge.marginal.V[1, 1] # sigma_x2^2
-    mu_x1 = node.in1.edge.marginal.m[1]
-    s_x1 = node.in1.edge.marginal.V[1, 1] # sigma_x1^2
+    mu_a = marg_slope.m[1]
+    s_a = marg_slope.V[1, 1] # sigma_a^2
+    mu_b = marg_offset.m[1]
+    s_b = marg_offset.V[1, 1] # sigma_b^2
+    mu_x2 = marg_out.m[1]
+    s_x2 = marg_out.V[1, 1] # sigma_x2^2
+    mu_x1 = marg_in1.m[1]
+    s_x1 = marg_in1.V[1, 1] # sigma_x1^2
+    a_s = marg_noise.a
+    b_s = marg_noise.b
 
-    if outbound_interface_id == 4 # s_N
-        dist_out.a = -0.5
-        dist_out.b = 0.5*((mu_x2 - mu_a*mu_x1 - mu_b)^2 - (mu_a^2)*(mu_x1^2) + (mu_a^2 + s_a)*(mu_x1^2 + s_x1) + s_b + s_x2)
+    if outbound_interface_id == 1 # in1
+        dist_out.m = [(mu_a*(mu_x2 - mu_b))/(s_a + mu_a^2)]
+        dist_out.V = reshape([(a_s + 1)/(b_s*(mu_a^2 + s_a))], 1, 1)
+        dist_out.W = nothing
+        dist_out.xi = nothing
+    elseif outbound_interface_id == 2 # a
+        dist_out.m = [(mu_x1*(mu_x2 - mu_b))/(s_x1 + mu_x1^2)]
+        dist_out.V = reshape([(a_s + 1)/(b_s*(mu_x1^2 + s_x1))], 1, 1)
+        dist_out.W = nothing
+        dist_out.xi = nothing
+    elseif outbound_interface_id == 3 # b
+        dist_out.m = [mu_x2 - mu_a*mu_x1]
+        dist_out.V = reshape([(a_s + 1)/b_s], 1, 1)
+        dist_out.W = nothing
+        dist_out.xi = nothing
+    elseif outbound_interface_id == 5 # out
+        dist_out.m = [mu_a*mu_x1 + mu_b]
+        dist_out.V = reshape([(a_s + 1)/b_s], 1, 1)
+        dist_out.W = nothing
+        dist_out.xi = nothing
     else
         error("Invalid outbound interface id $(outbound_interface_id), on $(typeof(node)) $(node.name).")
     end
@@ -234,33 +197,78 @@ function updateNodeMessage!(outbound_interface_id::Int,
     return node.interfaces[outbound_interface_id].message
 end
 
-function updateNodeMessage!(outbound_interface_id::Int,
-                            node::LinearCompositeNode,
-                            inbound_messages_value_types::Type{GaussianDistribution},
-                            outbound_message_value_type::Type{GammaDistribution})
+function updateNodeMessage!(node::LinearCompositeNode,
+                            outbound_interface_id::Int,
+                            outbound_message_value_type::Type{GammaDistribution},
+                            marg_in1::GaussianDistribution,
+                            marg_slope::GaussianDistribution,
+                            marg_offset::GaussianDistribution,
+                            ::Any,
+                            marg_out::GaussianDistribution)
     # Variational update function, takes the marginals as input.
+    # Sends to precision parameterized noise.
     # Derivation for the update rule can be found in the derivations notebook.
 
     dist_out = getOrCreateMessage(node.interfaces[outbound_interface_id], outbound_message_value_type).value
 
     # Ensure right parameterization
-    for i = [1, 2, 3, 5] # just get all of them, all marginals need to be defined anyway
-        ensureMWParametrization!(node.interfaces[i].edge.marginal)
+    for param = [marg_in1, marg_slope, marg_offset, marg_out] # just get all of them, all marginals need to be defined anyway
+        ensureMWParametrization!(param)
     end
 
     # Get the variables beforehand for more readable update equations
-    mu_a = node.slope.edge.marginal.m[1]
-    gam_a = node.slope.edge.marginal.W[1, 1] # gamma_a
-    mu_b = node.offset.edge.marginal.m[1]
-    gam_b = node.offset.edge.marginal.W[1, 1] # gamma_b
-    mu_x2 = node.out.edge.marginal.m[1]
-    gam_x2 = node.out.edge.marginal.W[1, 1] # gamma_x2
-    mu_x1 = node.in1.edge.marginal.m[1]
-    gam_x1 = node.in1.edge.marginal.W[1, 1] # gamma_x1
+    mu_a = marg_slope.m[1]
+    gam_a = marg_slope.W[1, 1] # gamma_a
+    mu_b = marg_offset.m[1]
+    gam_b = marg_offset.W[1, 1] # gamma_b
+    mu_x2 = marg_out.m[1]
+    gam_x2 = marg_out.W[1, 1] # gamma_x2
+    mu_x1 = marg_in1.m[1]
+    gam_x1 = marg_in1.W[1, 1] # gamma_x1
 
     if outbound_interface_id == 4 # noise_N
         dist_out.a = 1.5
         dist_out.b = 0.5*((mu_x2 - mu_a*mu_x1 - mu_b)^2 - (mu_a^2)*(mu_x1^2) + (mu_a^2 + inv(gam_a))*(mu_x1^2 + inv(gam_x1)) + inv(gam_b) + inv(gam_x2))
+    else
+        error("Invalid outbound interface id $(outbound_interface_id), on $(typeof(node)) $(node.name).")
+    end
+
+    # Set the outbound message
+    return node.interfaces[outbound_interface_id].message
+end
+
+function updateNodeMessage!(node::LinearCompositeNode,
+                            outbound_interface_id::Int,
+                            outbound_message_value_type::Type{InverseGammaDistribution},
+                            marg_in1::GaussianDistribution,
+                            marg_slope::GaussianDistribution,
+                            marg_offset::GaussianDistribution,
+                            ::Any,
+                            marg_out::GaussianDistribution)
+    # Variational update function, takes the marginals as input.
+    # Sends to variance parameterized noise.
+    # Derivation for the update rule can be found in the derivations notebook.
+
+    dist_out = getOrCreateMessage(node.interfaces[outbound_interface_id], outbound_message_value_type).value
+
+    # Ensure right parameterization
+    for param = [marg_in1, marg_slope, marg_offset, marg_out] # just get all of them, all marginals need to be defined anyway
+        ensureMVParametrization!(param)
+    end
+
+    # Get the variables beforehand for more readable update equations
+    mu_a = marg_slope.m[1]
+    s_a = marg_slope.V[1, 1] # sigma_a^2
+    mu_b = marg_offset.m[1]
+    s_b = marg_offset.V[1, 1] # sigma_b^2
+    mu_x2 = marg_out.m[1]
+    s_x2 = marg_out.V[1, 1] # sigma_x2^2
+    mu_x1 = marg_in1.m[1]
+    s_x1 = marg_in1.V[1, 1] # sigma_x1^2
+
+    if outbound_interface_id == 4 # s_N
+        dist_out.a = -0.5
+        dist_out.b = 0.5*((mu_x2 - mu_a*mu_x1 - mu_b)^2 - (mu_a^2)*(mu_x1^2) + (mu_a^2 + s_a)*(mu_x1^2 + s_x1) + s_b + s_x2)
     else
         error("Invalid outbound interface id $(outbound_interface_id), on $(typeof(node)) $(node.name).")
     end
