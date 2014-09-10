b7c9497832e2a31a4716ecf79fe8ab265f16501c
Marco Cox <mail@marcocox.com>
2014-09-10 15:44:54 +0200

    Fixes in demo tester

--- test/test_demos.jl
+++ test/test_demos.jl
@@ -3,11 +3,22 @@
 
 orig_pwd = pwd()
 demo_dir = normpath(joinpath(dirname(@__FILE__()), "..", "demo"))
+forneylab_main = joinpath(normpath(joinpath(dirname(@__FILE__()), "..", "src")), "ForneyLab.jl")
 cd(demo_dir)
 
 # Convert all iJulia notebooks in the demo directory to regular .jl files
 run(`ipython nbconvert --profile julia --to python *.ipynb`)
-map(file -> mv(file, file[1:end-3]*".jl"), filter!(r"\.py$", readdir(demo_dir))) # Rename files from .py to .jl
+for file in  filter!(r"\.py$", readdir(demo_dir))
+    # Generate Julia script from .py file
+    open(file[1:end-3]*".jl", "w") do f
+        # Insert a line to include ForneyLab
+        write(f, "include(\"$(forneylab_main)\")\n")
+        # Write the actual Julia script
+        write(f, readall(file))
+    end
+    # Remove .py file
+    rm(file)
+end
 
 # Run the demos
 try
