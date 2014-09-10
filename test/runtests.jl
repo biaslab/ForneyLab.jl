db7db00ee49529c387ba24efd224b65ec93b9bd9
Marco Cox <mail@marcocox.com>
2014-09-10 16:00:41 +0200

    Added test/runtests.jl so that Pkg.test("ForneyLab") works

---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+# Run the tests for ForneyLab
+# This file is automatically executed if you call Pkg.test("ForneyLab")
+
+include("../src/ForneyLab.jl")
+using ForneyLab
+include("test_forneylab.jl")
