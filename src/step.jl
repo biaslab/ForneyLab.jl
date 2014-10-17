ca29fa192a0367837619dd313b4c293620f8733b
Marco Cox <mail@marcocox.com>
2014-10-17 11:56:03 +0200

    Added step-related tests and some extra checks

--- src/step.jl
+++ src/step.jl
@@ -1,9 +1,19 @@
 export setReadBuffer, setWriteBuffer, clearBuffers!, addTimeWrap, clearTimeWraps!
 
-setReadBuffer(node::TerminalNode, buffer::Vector, graph::FactorGraph=getCurrentGraph()) = (graph.read_buffers[node] = buffer)
+function setReadBuffer(node::TerminalNode, buffer::Vector, graph::FactorGraph=getCurrentGraph())
+    (node in getNodes(graph)) || error("The specified node is not part of the current or specified graph")
+    graph.read_buffers[node] = buffer
+end
 
-setWriteBuffer(interface::Interface, buffer::Vector, graph::FactorGraph=getCurrentGraph()) = (graph.write_buffers[interface] = buffer) # Write buffer for message
-setWriteBuffer(edge::Edge, buffer::Vector, graph::FactorGraph=getCurrentGraph()) = (graph.write_buffers[edge] = buffer) # Write buffer for marginal
+function setWriteBuffer(interface::Interface, buffer::Vector, graph::FactorGraph=getCurrentGraph())
+    (interface.node in getNodes(graph)) || error("The specified interface is not part of the current or specified graph")
+    graph.write_buffers[interface] = buffer # Write buffer for message
+end
+
+function setWriteBuffer(edge::Edge, buffer::Vector, graph::FactorGraph=getCurrentGraph())
+    (edge in getEdges(graph)) || error("The specified edge is not part of the current or specified graph")
+    graph.write_buffers[edge] = buffer # Write buffer for marginal
+end
 
 function clearBuffers!(graph::FactorGraph=getCurrentGraph())
     graph.read_buffers = Dict{TerminalNode, Vector}()
@@ -12,6 +22,8 @@ function clearBuffers!(graph::FactorGraph=getCurrentGraph())
 end
 
 function addTimeWrap(from::TerminalNode, to::TerminalNode, graph::FactorGraph=getCurrentGraph())
+    (from in getNodes(graph)) || error("The specified 'from' node is not part of the current or specified graph")
+    (to in getNodes(graph)) || error("The specified 'to' node is not part of the current or specified graph")
     !is(from, to) || error("Cannot create time wrap: from and to must be different nodes")
     # Verify that from and to are not already in a time wrap
     for time_wrap in graph.time_wraps
--- test/test_step.jl
+++ test/test_step.jl
@@ -1,1 +1,76 @@
-# TODO: implement tests here
+#####################
+# Integration tests
+#####################
+
+facts("Read/write buffer integration tests") do
+    # Background
+    g = FactorGraph()
+    node_t1 = TerminalNode()
+    node_t2 = TerminalNode()
+    e = Edge(node_t1, node_t2)
+
+    # setReadBuffer
+    context("setReadBuffer should register a read buffer for a TerminalNode") do
+        read_buffer = zeros(10)
+        setReadBuffer(node_t1, read_buffer)
+        @fact g.read_buffers[node_t1] => read_buffer
+    end
+    context("setReadBuffer should not register a read buffer if the specified node is not in the specified graph") do
+        @fact_throws setReadBuffer(TerminalNode(), zeros(10))
+    end
+
+    # setWriteBuffer
+    context("setWriteBuffer should register a write buffer for an Interface") do
+        write_buffer = Array(Any, 0)
+        setWriteBuffer(node_t1.out, write_buffer)
+        @fact g.write_buffers[node_t1.out] => write_buffer
+    end
+    context("setWriteBuffer should register a write buffer for an Edge (marginal)") do
+        write_buffer = Array(Any, 0)
+        setWriteBuffer(e, write_buffer)
+        @fact g.write_buffers[e] => write_buffer
+    end
+    context("setWriteBuffer should not register a write buffer if the specified interface/edge is not in the specified graph") do
+        write_buffer = Array(Any, 0)
+        @fact_throws setWriteBuffer(TerminalNode().out, write_buffer)
+        g2 = FactorGraph()
+        @fact_throws setWriteBuffer(e, write_buffer)
+    end
+
+    # clearBuffers!
+    setCurrentGraph(g)
+    context("clearBuffers! should deregister all read/write buffers") do
+        clearBuffers!(g)
+        @fact length(g.read_buffers) => 0
+        @fact length(g.write_buffers) => 0
+    end
+end
+
+facts("TimeWrap integration tests") do
+    # Background
+    g = FactorGraph()
+    node_t1 = TerminalNode()
+    node_t2 = TerminalNode()
+    e = Edge(node_t1, node_t2)
+
+    # addTimeWrap
+    context("addTimeWrap should not register a timewrap if the nodes do not belong to the specified graph or if the nodes are the same") do
+        @fact_throws addTimeWrap(TerminalNode(), node_t1)
+        @fact_throws addTimeWrap(node_t2, TerminalNode())
+        @fact_throws addTimeWrap(node_t1, node_t1)
+    end
+    context("addTimeWrap should register a timewrap for a pair of TerminalNodes") do
+        time_wraps = addTimeWrap(node_t1, node_t2)
+        @fact length(time_wraps) => 1
+        @fact ((node_t1, node_t2) in time_wraps) => true
+    end
+    context("addTimeWrap should refuse to register multiple timewraps per TerminalNode") do
+        @fact_throws addTimeWrap(node_t1, node_t2)
+    end
+
+    # clearTimeWraps!
+    context("clearTimeWraps! should deregister all time wraps") do
+        clearTimeWraps!(g)
+        @fact length(g.time_wraps) => 0
+    end
+end
