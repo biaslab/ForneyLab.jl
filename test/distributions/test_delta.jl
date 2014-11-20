fac2fbc5721bf86de9ea85205db8a1566286147e
Marco Cox <mail@marcocox.com>
2014-11-20 16:39:18 +0100

    Added DeltaDistribution test file

---
+++ test/distributions/test_delta.jl
@@ -0,0 +1,24 @@
+#####################
+# Unit tests
+#####################
+
+facts("DeltaDistribution unit tests") do
+    context("DeltaDistribution() should initialize a delta distribution") do
+        @fact DeltaDistribution().m => 1.0
+        @fact DeltaDistribution(2.0).m => 2.0
+        @fact DeltaDistribution([2.0]).m => [2.0]
+        @fact DeltaDistribution(reshape([2.0],1,1)).m => reshape([2.0],1,1)
+        @fact DeltaDistribution(:something).m => :something
+    end
+
+    context("There should be no such thing as uninformative(DeltaDistribution)") do
+        @fact_throws uninformative(DeltaDistribution)
+        @fact_throws uninformative(DeltaDistribution{Float64})
+    end
+
+    context("Numbers, symbols, and arrays should convert to DeltaDistribution") do
+        @fact convert(ProbabilityDistribution, 3.0) => DeltaDistribution(3.0)
+        @fact convert(ProbabilityDistribution, [3.0, 4.0]) => DeltaDistribution([3.0, 4.0])
+        @fact convert(ProbabilityDistribution, :something) => DeltaDistribution(:something)
+    end
+end
