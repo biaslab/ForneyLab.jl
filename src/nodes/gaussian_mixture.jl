1027b3b6b3902c8e1287fcd4a2f819098856c1cf
AnoukvDiepen <anoukvdiepen@gmail.com>
2016-05-03 17:31:33 +0200

    Fixed bugs in multivar. GMN, but not all

--- src/nodes/gaussian_mixture.jl
+++ src/nodes/gaussian_mixture.jl
@@ -79,23 +79,23 @@ end
 
 # VMP message towards i[:pi]
 # Multivariate gaussian with two clusters
-function variationalRule!(  node::GaussianMixtureNode,
+function variationalRule!{dims}(  node::GaussianMixtureNode,
                             ::Type{Val{1}},
                             outbound_dist::BetaDistribution,
                             ::Any,
-                            q_m1::MvGaussianDistribution,
-                            q_w1::WishartDistribution,
-                            q_m2::MvGaussianDistribution,
-                            q_w2::WishartDistribution,
-                            q_x::MvGaussianDistribution,
+                            q_m1::MvGaussianDistribution{dims},
+                            q_w1::WishartDistribution{dims},
+                            q_m2::MvGaussianDistribution{dims},
+                            q_w2::WishartDistribution{dims},
+                            q_x::MvGaussianDistribution{dims},
                             q_z::BernoulliDistribution)
 
     ensureParameters!(q_m1, (:m, :V))
 
-
+    print("start pi\n")
     outbound_dist.a   =   q_z.p+1.
     outbound_dist.b   =   2.-q_z.p
-
+    print("end pi\n")
     return outbound_dist
 end
 
@@ -123,22 +123,23 @@ end
 
 # VMP message towards i[:m1]
 # Multivariate Gaussian with two clusters
-function variationalRule!(  node::GaussianMixtureNode,
+function variationalRule!{dims}(  node::GaussianMixtureNode,
                             ::Type{Val{2}},
-                            outbound_dist::MvGaussianDistribution,
+                            outbound_dist::MvGaussianDistribution{dims},
                             q_pi::BetaDistribution,
                             ::Any,
-                            q_w1::WishartDistribution,
-                            q_m2::MvGaussianDistribution,
-                            q_w2::WishartDistribution,
-                            q_x::MvGaussianDistribution,
+                            q_w1::WishartDistribution{dims},
+                            q_m2::MvGaussianDistribution{dims},
+                            q_w2::WishartDistribution{dims},
+                            q_x::MvGaussianDistribution{dims},
                             q_z::BernoulliDistribution)
     ensureParameters!(q_x, (:m, :V))
-
+    print("start m1\n")
     outbound_dist.m   =   q_x.m
-    outbound_dist.V   =   NaN
-    outbound_dist.xi  =   NaN
+    invalidate!(outbound_dist.V)
+    invalidate!(outbound_dist.xi)
     outbound_dist.W   =   q_z.p*q_w1.nu*q_w1.V
+    print("end m1\n")
 
     return outbound_dist
 end
@@ -168,23 +169,32 @@ end
 
 # VMP message towards i[:w1]
 # Multivariate Gaussian with two clusters
-function variationalRule!(  node::GaussianMixtureNode,
+function variationalRule!{dims}(  node::GaussianMixtureNode,
                             ::Type{Val{3}},
-                            outbound_dist::WishartDistribution,
+                            outbound_dist::WishartDistribution{dims},
                             q_pi::BetaDistribution,
-                            q_m1::MvGaussianDistribution,
+                            q_m1::MvGaussianDistribution{dims},
                             ::Any,
-                            q_m2::MvGaussianDistribution,
-                            q_w2::WishartDistribution,
-                            q_x::MvGaussianDistribution,
+                            q_m2::MvGaussianDistribution{dims},
+                            q_w2::WishartDistribution{dims},
+                            q_x::MvGaussianDistribution{dims},
                             q_z::BernoulliDistribution)
-
+    print("startw1\n")
     ensureParameters!(q_m1, (:m, :V))
     ensureParameters!(q_x, (:m, :V))
-
-    outbound_dist.nu    =   1.+q_z.p+length(q_x.m)
-    outbound_dist.V     =   inv(q_z.p*(q_x-q_m1.m)*transpose(q_x-q_m1.m))
-
+    b=q_x.m-q_m1.m
+    print(b,"\n")
+    outbound_dist.nu    =   1.+q_z.p+dims
+    #print("This is the matrix",inv(q_z.p*(q_x.m-q_m1.m)*transpose(q_x.m-q_m1.m)),"\n")
+    #print("determinant: ",det(q_z.p*(q_x.m-q_m1.m)*transpose(q_x.m-q_m1.m)), "\n")
+    #if det(q_z.p*(q_x.m-q_m1.m)*transpose(q_x.m-q_m1.m)) <tiny
+    #  outbound_dist.V = inv(diagm(tiny*ones(dims)))
+    #else
+      gausterm1=(q_x.m*transpose(q_x.m)-q_x.m*transpose(q_m1.m)-(q_m1.m)*transpose(q_x.m)+q_m1.V+q_m1.m*transpose(q_m1.m))
+      outbound_dist.V     =   inv(q_z.p*gausterm1)
+    #end
+    print(outbound_dist.V)
+    print("endw1\n")
     return outbound_dist
 end
 
@@ -213,23 +223,23 @@ end
 
 # VMP message towards i[:m2]
 # Multivariate Gaussian with two clusters
-function variationalRule!(  node::GaussianMixtureNode,
+function variationalRule!{dims}(  node::GaussianMixtureNode,
                             ::Type{Val{4}},
-                            outbound_dist::MvGaussianDistribution,
+                            outbound_dist::MvGaussianDistribution{dims},
                             q_pi::BetaDistribution,
-                            q_m1::MvGaussianDistribution,
-                            q_w1::WishartDistribution,
+                            q_m1::MvGaussianDistribution{dims},
+                            q_w1::WishartDistribution{dims},
                             ::Any,
-                            q_w2::WishartDistribution,
-                            q_x::MvGaussianDistribution,
+                            q_w2::WishartDistribution{dims},
+                            q_x::MvGaussianDistribution{dims},
                             q_z::BernoulliDistribution)
     ensureParameters!(q_x, (:m, :V))
-
+    print("start m2\n")
     outbound_dist.m   =   q_x.m
-    outbound_dist.V   =   NaN
-    outbound_dist.xi  =   NaN
-    outbound_dist.W   =   (1.-q_z.p)*q_w2.nu/q_w2.V
-
+    invalidate!(outbound_dist.V)
+    invalidate!(outbound_dist.xi)
+    outbound_dist.W   =   (1.-q_z.p)*q_w2.nu*q_w2.V
+    print("end m2\n")
     return outbound_dist
 end
 
@@ -258,23 +268,29 @@ end
 
 # VMP message towards i[:w2]
 # Multivariate Gaussian with two clusters
-function variationalRule!(  node::GaussianMixtureNode,
+function variationalRule!{dims}(  node::GaussianMixtureNode,
                             ::Type{Val{5}},
-                            outbound_dist::WishartDistribution,
+                            outbound_dist::WishartDistribution{dims},
                             q_pi::BetaDistribution,
-                            q_m1::MvGaussianDistribution,
-                            q_w1::WishartDistribution,
-                            q_m2::MvGaussianDistribution,
+                            q_m1::MvGaussianDistribution{dims},
+                            q_w1::WishartDistribution{dims},
+                            q_m2::MvGaussianDistribution{dims},
                             ::Any,
-                            q_x::MvGaussianDistribution,
+                            q_x::MvGaussianDistribution{dims},
                             q_z::BernoulliDistribution)
 
     ensureParameters!(q_m2, (:m, :V))
     ensureParameters!(q_x, (:m, :V))
-
-    outbound_dist.nu    =   1.+(1.-q_z.p)+length(q_x.m)
-    outbound_dist.V     =   inv((1-q_z.p)*(q_x.m-q_m2.m)*transpose(q_x.m-q_m2.m))
-
+    print("start w2\n")
+    outbound_dist.nu    =   1.+(1.-q_z.p)+dims
+    #if det(q_z.p*(q_x.m-q_m2.m)*transpose(q_x.m-q_m2.m)) <tiny
+    #  outbound_dist.V = inv(diagm(tiny*ones(dims)))
+    #else
+      gausterm2=(q_x.m*transpose(q_x.m)-q_x.m*transpose(q_m2.m)-q_m2.m*transpose(q_x.m)+q_m2.V+q_m2.m*transpose(q_m2.m))
+      outbound_dist.V     =   inv((1.-q_z.p)*gausterm2)
+    #end
+    print(outbound_dist.V, "\n")
+    print("end w2\n")
     return outbound_dist
 end
 
@@ -298,14 +314,14 @@ function variationalRule!(  node::GaussianMixtureNode,
     #calculating ln(ro1)
     e_ln_pi1    =   digamma(q_pi.a)-digamma(q_pi.a+q_pi.b)
     e_ln_w1     =   digamma(q_w1.a)-log(q_w1.b)
-    e_m1_square =   q_x.m^2-2*q_x.m*q_m1.m+q_m1.V+q_m1.m^2
+    e_m1_square =   q_x.m^2-2.0*q_x.m*q_m1.m+q_m1.V+q_m1.m^2
     ln_ro1      =   e_ln_pi1+0.5*e_ln_w1-0.5*log(2pi)-0.5*q_w1.a/q_w1.b*e_m1_square
 
     #calculating ln(ro2) for normalization
 
     e_ln_pi2    =   digamma(q_pi.b)-digamma(q_pi.b+q_pi.a)
     e_ln_w2     =   digamma(q_w2.a)-log(q_w2.b)
-    e_m2_square =   q_x.m^2-2*q_x.m*q_m2.m+q_m2.V+q_m2.m^2
+    e_m2_square =   q_x.m^2-2.0*q_x.m*q_m2.m+q_m2.V+q_m2.m^2
     ln_ro2      =   e_ln_pi2+0.5*e_ln_w2-0.5*log(2pi)-0.5*q_w2.a/q_w2.b*e_m2_square
 
     outbound_dist.p = exp(ln_ro1)/(exp(ln_ro1)+exp(ln_ro2))
@@ -315,36 +331,64 @@ end
 
 # VMP message towards i[:z]
 # Multivariate Gaussian with two clusters
-function variationalRule!(  node::GaussianMixtureNode,
+function variationalRule!{dims}(node::GaussianMixtureNode,
                             ::Type{Val{7}},
                             outbound_dist::BernoulliDistribution,
                             q_pi::BetaDistribution,
-                            q_m1::MvGaussianDistribution,
-                            q_w1::WishartDistribution,
-                            q_m2::MvGaussianDistribution,
-                            q_w2::WishartDistribution,
-                            q_x::MvGaussianDistribution,
+                            q_m1::MvGaussianDistribution{dims},
+                            q_w1::WishartDistribution{dims},
+                            q_m2::MvGaussianDistribution{dims},
+                            q_w2::WishartDistribution{dims},
+                            q_x::MvGaussianDistribution{dims},
                             ::Any)
 
     ensureParameters!(q_m1, (:m, :V))
     ensureParameters!(q_x, (:m, :V))
     ensureParameters!(q_m2, (:m, :V))
-
+    print("start z\n")
     #calculating ln(ro1)
     e_ln_pi1      =   digamma(q_pi.a)-digamma(q_pi.a+q_pi.b)
-    e_ln_w1       =   digamma(q_w1.nu/2)-length(q_x.m)*log(2)+log(det(q_w1.V))
+    print("1\n")
+
+    #calculating multivariate digamma function
+    multidi1      =   0.0
+    for i=1:dims
+      multidi1      =   multidi1+digamma((q_w1.nu-i+1)/2)
+    end
+
+    e_ln_w1       =   multidi1+dims*log(2.0)+log(det(q_w1.V))
+      print("2\n")
     e_w1          =   q_w1.nu*q_w1.V
-    ln_ro1        =   e_ln_pi1+0.5*e_ln_w1+length(q_x.m)/2*log(2*pi)-0.5*transpose(q_x.m-q_m1.m)*e_w1*(q_x.m-q_m1.m)
+      print("3\n")
+    gausterm1=trace((q_x.m*transpose(q_x.m)-q_x.m*transpose(q_m1.m)-q_m1.m*transpose(q_x.m)+q_m1.V+q_m1.m*transpose(q_m1.m))*e_w1)
+
+    ln_ro1        =   e_ln_pi1+0.5*e_ln_w1+dims/2.0*log(2.0*pi)-0.5*gausterm1
+      print("4\n")
+      print(ln_ro1,"\n")
 
     #calculating ln(ro2) for normalization
 
     e_ln_pi2      =   digamma(q_pi.b)-digamma(q_pi.b+q_pi.a)
-    e_ln_w2       =   digamma(q_w2.a)-log(q_w2.b)
-    e_w1          =   q_w2.nu*q_w2.V
-    ln_ro2        =   e_ln_pi2+0.5*e_ln_w2+length(q_x.m)/2*log(2*pi)-0.5*transpose(q_x.m-q_m2.m)*e_w2*(q_x.m-q_m2.m)
+    print("5\n")
+    #multivariate digamma
+    multidi2      =   0.0
+    for i=1:dims
+      multidi2      =   multidi2+digamma((q_w2.nu-i+1)/2)
+    end
 
-    outbound_dist.p = exp(ln_ro1)/(exp(ln_ro1)+exp(ln_ro2))
 
+    e_ln_w2       =  multidi2+dims*log(2.0)+log(det(q_w2.V)) #eerste digamma moet eigenlijk multivariate digamma worden
+    print("6\n")
+    e_w2          =   q_w2.nu*q_w2.V
+    print("7\n")
+    gausterm2=trace((q_x.m*transpose(q_x.m)-q_x.m*transpose(q_m2.m)-q_m2.m*transpose(q_x.m)+q_m2.V+q_m2.m*transpose(q_m2.m))*e_w2)
+
+    ln_ro2        =   e_ln_pi2+0.5*e_ln_w2+dims/2.0*log(2.0*pi)-0.5*gausterm2
+    print("8\n")
+    print(ln_ro2,"\n")
+    outbound_dist.p = exp(ln_ro1)/(exp(ln_ro1)+exp(ln_ro2)) #deze term wordt te klein!!!
+    print("this is the outbound message", outbound_dist.p,"\n")
+    print("end z \n")
     return outbound_dist
 end
 
@@ -369,21 +413,21 @@ function variationalRule!(  node::GaussianMixtureNode,s
 end
 
 # VMP message towards i[:x]
-function variationalRule!(  node::GaussianMixtureNode,
+function variationalRule!{dims}(  node::GaussianMixtureNode,
                             ::Type{Val{6}},
-                            outbound_dist::MvGaussianDistribution,
+                            outbound_dist::MvGaussianDistribution{dims},
                             q_pi::BetaDistribution,
-                            q_m1::MvGaussianDistribution,
-                            q_w1::WishartDistribution,
-                            q_m2::MvGaussianDistribution,
-                            q_w2::WishartDistribution,
+                            q_m1::MvGaussianDistribution{dims},
+                            q_w1::WishartDistribution{dims},
+                            q_m2::MvGaussianDistribution{dims},
+                            q_w2::WishartDistribution{dims},
                             q_x::Any,
                             q_z::BernoulliDistribution)
-
-    outbound_dist.m=[0.0, 0.0]
-    outbound_dist.V=diagonal([100,100])
-    outbound_dist.xi=NaN
-    outbound_dist.W=NaN
-
+    print("start x\n")
+    outbound_dist.m = zeros(dims)
+    outbound_dist.V = diagm(100.0*ones(dims))
+    invalidate!(outbound_dist.xi)
+    invalidate!(outbound_dist.W)
+    print("end x\n")
     return outbound_dist
 end
