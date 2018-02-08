464510796fdb11f2f2f1dcf916d7e6270c431144
Marco Cox <mail@marcocox.com>
2018-02-08 08:30:38 +0100

    Make messages in draw() stand out by making them bold and blue

--- src/visualization.jl
+++ src/visualization.jl
@@ -143,9 +143,19 @@ function edgeDot(edge::Edge; msg_labels=Dict{Interface, String}(), is_external_e
     if is_external_edge
         dot *= "[taillabel=\"$(b_label)\", headlabel=\"$(a_label)\", style=\"dashed\" color=\"black\"]\n"
     else
-        msg_label_a = (haskey(msg_labels, edge.a) ? msg_labels[edge.a] : "")
-        msg_label_b = (haskey(msg_labels, edge.b) ? msg_labels[edge.b] : "")
-        dot *= "[taillabel=\"$(b_label)\n$(msg_label_b)\", headlabel=\"$(a_label)\n$(msg_label_a)\" color=\"black\"]"
+        msg_label_a = haskey(msg_labels, edge.a) ? "<B><FONT COLOR=\"blue\">"*msg_labels[edge.a]*"</FONT></B>" : ""
+        msg_label_b = haskey(msg_labels, edge.b) ? "<B><FONT COLOR=\"blue\">"*msg_labels[edge.b]*"</FONT></B>" : ""
+
+        props = String[]
+        if !isempty(b_label) || !isempty(msg_label_b)
+            push!(props, "taillabel=<$(b_label) \n$(msg_label_b)>")
+        end
+        if !isempty(a_label) || !isempty(msg_label_a)
+            push!(props, "taillabel=<$(a_label) \n$(msg_label_a)>")
+        end
+        if !isempty(props)
+            dot *= "[$(join(props,", "))]"
+        end
     end
 end
 
