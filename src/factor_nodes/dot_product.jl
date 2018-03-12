9871ce1a64ad61e173af0fc72f101483fc7d7b4f
Marco Cox <mail@marcocox.com>
2018-03-12 11:28:27 +0100

    Add dot product node

---
+++ src/factor_nodes/dot_product.jl
@@ -0,0 +1,43 @@
+export DotProduct
+
+"""
+Description:
+
+    out = in1'*in2
+
+    in1: d-dimensional vector
+    in2: d-dimensional vector
+    out: scalar
+
+           in2
+           |
+      in1  V   out
+    ----->[⋅]----->
+
+    f(in1, in2, out) =  δ(out - in1'*in2)
+
+Interfaces:
+
+    1 i[:in1], 2 i[:in2], 3 i[:out]
+
+Construction:
+
+    DotProduct(out, in1, in2, id=:my_node)
+"""
+mutable struct DotProduct <: DeltaFactor
+    id::Symbol
+    interfaces::Vector{Interface}
+    i::Dict{Symbol,Interface}
+
+    function DotProduct(out::Variable, in1::Variable, in2::Variable; id=generateId(DotProduct))
+        self = new(id, Array{Interface}(3), Dict{Int,Interface}())
+        addNode!(currentGraph(), self)
+        self.i[:out] = self.interfaces[1] = associate!(Interface(self), out)
+        self.i[:in1] = self.interfaces[2] = associate!(Interface(self), in1)
+        self.i[:in2] = self.interfaces[3] = associate!(Interface(self), in2)
+
+        return self
+    end
+end
+
+slug(::Type{DotProduct}) = "dot"
