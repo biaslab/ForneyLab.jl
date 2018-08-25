d724a9ed13a8cf7f390d47c0da47669e2f6c77ea
Ivan Bocharov <bocharovia@gmail.com>
2018-08-25 18:11:42 +0200

    @symmetrical macro now properly accepts parametric methods.

--- src/helpers.jl
+++ src/helpers.jl
@@ -129,20 +129,31 @@ Example:
     end
 """
 macro symmetrical(orig::Expr)
-    # TODO: Fix the swap
-    # (orig.args[1].head == :call) || error("Invalid use of @symmetrical")
-    # eval(orig)
-    # swap_arg_indexes = Int64[]
-    # for i=1:length(orig.args[1].args)
-    #     (typeof(orig.args[1].args[i]) == Expr) || continue
-    #     (orig.args[1].args[i].head == :(::)) || continue
-    #     push!(swap_arg_indexes, i)
-    #     (length(swap_arg_indexes) < 2) || break
-    # end
+    if orig.args[1].head == :where
+        eval(orig)
+        mirrored = deepcopy(orig)
+        mirrored.args[1] = swap_arguments(orig.args[1])
+    elseif orig.args[1].head == :call
+        eval(orig)
+        mirrored = swap_arguments(orig)
+    else
+        error("Invalid use of @symmetrical")
+    end
+    eval(mirrored)
+end
 
-    # mirrored = deepcopy(orig)
-    # mirrored.args[1].args[swap_arg_indexes] = orig.args[1].args[reverse(swap_arg_indexes)]
-    # eval(mirrored)
+function swap_arguments(orig::Expr)
+    swap_arg_indexes = Int64[]
+    for i=1:length(orig.args[1].args)
+        (typeof(orig.args[1].args[i]) == Expr) || continue
+        (orig.args[1].args[i].head == :(::)) || continue
+        push!(swap_arg_indexes, i)
+        (length(swap_arg_indexes) < 2) || break
+    end
+
+    mirrored = deepcopy(orig)
+    mirrored.args[1].args[swap_arg_indexes] = orig.args[1].args[reverse(swap_arg_indexes)]
+    return mirrored
 end
 
 """
