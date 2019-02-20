b06cd853dc1217acad10bdde162fc35a2be22656
Martin Roa Villescas <mroavi@gmail.com>
2019-02-20 16:45:11 +0100

    Add two sum-product update rules for the Bernoulli factor node

--- src/update_rules/bernoulli.jl
+++ src/update_rules/bernoulli.jl
@@ -3,6 +3,16 @@
                 :inbound_types => (Nothing, Message{PointMass}),
                 :name          => SPBernoulliOutVP)
 
+@sumProductRule(:node_type     => Bernoulli,
+                :outbound_type => Message{Beta},
+                :inbound_types => (Message{PointMass}, Nothing),
+                :name          => SPBernoulliIn1PV)
+
+@sumProductRule(:node_type     => Bernoulli,
+                :outbound_type => Message{Bernoulli},
+                :inbound_types => (Nothing, Message{Beta}),
+                :name          => SPBernoulliOutVB)
+
 @naiveVariationalRule(:node_type     => Bernoulli,
                       :outbound_type => Message{Bernoulli},
                       :inbound_types => (Nothing, ProbabilityDistribution),
