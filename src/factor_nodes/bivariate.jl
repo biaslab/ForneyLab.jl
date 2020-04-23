c4108f45ce8425a8e40b29404c010a9e163e5910
Ivan Bocharov <bocharovia@gmail.com>
2020-04-23 20:20:44 +0200

    Added number of samples to the node constructor.

--- src/factor_nodes/bivariate.jl
+++ src/factor_nodes/bivariate.jl
@@ -23,10 +23,11 @@ mutable struct Bivariate{T<:ApproximationMethod} <: DeltaFactor
     g::Function # Vector function that expresses the output vector as a function of the input vector; reduces to scalar for 1-d
     dims::Tuple # Dimension of breaker message on input interface
     status::Dict{Symbol, Union{Bool, Message}} #Keeps the status of node to ensure that input variables are updated simultaneously
+    n_samples::Int # Number of samples for Laplace sampling
 
-    function Bivariate{Laplace}(out, in1, in2, g::Function; dims=(), id=ForneyLab.generateId(Bivariate{Laplace}))
+    function Bivariate{Laplace}(out, in1, in2, g::Function; dims=(), n_samples=1000, id=ForneyLab.generateId(Bivariate{Laplace}))
         @ensureVariables(out, in1, in2)
-        self = new(id, Vector{Interface}(undef, 3), Dict{Symbol,Interface}(), g, dims, Dict(:updated=>false))
+        self = new(id, Vector{Interface}(undef, 3), Dict{Symbol,Interface}(), g, dims, Dict(:updated=>false), n_samples)
         ForneyLab.addNode!(currentGraph(), self)
         self.i[:out] = self.interfaces[1] = associate!(Interface(self), out)
         self.i[:in1] = self.interfaces[2] = associate!(Interface(self), in1)
--- src/factor_nodes/nonlinear.jl
+++ src/factor_nodes/nonlinear.jl
@@ -37,6 +37,7 @@ mutable struct Nonlinear{T<:ApproximationMethod} <: DeltaFactor
     g_inv::Union{Function, Nothing, Vector} # Inverse of g with respect to individual inbounds (optional)
     alpha::Union{Float64, Nothing} # Spread parameter for unscented transform
     dims::Union{Tuple, Vector} # Dimension of breaker message(s) on input interface(s)
+    n_samples::Int # Number of samples for Laplace sampling
 
     function Nonlinear{Unscented}(out, args::Vararg; g::Function, g_inv=nothing, alpha=nothing, dims=(), id=ForneyLab.generateId(Nonlinear{Unscented}))
         @ensureVariables(out)
@@ -44,7 +45,7 @@ mutable struct Nonlinear{T<:ApproximationMethod} <: DeltaFactor
         for i=1:n_args
             @ensureVariables(args[i])
         end
-        self = new(id, Vector{Interface}(undef, n_args+1), Dict{Symbol,Interface}(), g, g_inv, alpha, dims)
+        self = new(id, Vector{Interface}(undef, n_args+1), Dict{Symbol,Interface}(), g, g_inv, alpha, dims, 0)
         ForneyLab.addNode!(currentGraph(), self)
         self.i[:out] = self.interfaces[1] = associate!(Interface(self), out)
         for k = 1:n_args
@@ -54,9 +55,9 @@ mutable struct Nonlinear{T<:ApproximationMethod} <: DeltaFactor
         return self
     end
 
-    function Nonlinear{Laplace}(out, in1, g::Function; id=ForneyLab.generateId(Nonlinear{Laplace}))
+    function Nonlinear{Laplace}(out, in1, g::Function; n_samples=1000, id=ForneyLab.generateId(Nonlinear{Laplace}))
         @ensureVariables(out, in1)
-        self = new(id, Vector{Interface}(undef, 2), Dict{Symbol,Interface}(), g, nothing, nothing, ())
+        self = new(id, Vector{Interface}(undef, 2), Dict{Symbol,Interface}(), g, nothing, nothing, (), n_samples)
         ForneyLab.addNode!(currentGraph(), self)
         self.i[:out] = self.interfaces[1] = associate!(Interface(self), out)
         self.i[:in1] = self.interfaces[2] = associate!(Interface(self), in1)
