f9af2e47ada9a0d7752fc08d65fc79781d95bc6f
semihakbayrak <semihakbayrak@gmail.com>
2020-06-18 15:50:33 +0300

    Allow inference with categorical observations

--- src/engines/julia/update_rules/categorical.jl
+++ src/engines/julia/update_rules/categorical.jl
@@ -1,13 +1,21 @@
 export
 ruleSPCategoricalOutNP,
+ruleSPCategoricalIn1,
 ruleVBCategoricalOut,
 ruleVBCategoricalIn1
 
 ruleSPCategoricalOutNP(msg_out::Nothing, msg_p::Message{PointMass, Multivariate}) = Message(Univariate, Categorical, p=deepcopy(msg_p.dist.params[:m]))
 
+ruleSPCategoricalIn1(msg_out::Message{PointMass, Multivariate}, msg_p::Nothing) = Message(Multivariate, Dirichlet, a=deepcopy(msg_out.dist.params[:m]).+ 1.0)
+
 function ruleVBCategoricalOut(marg_out::Any, marg_p::ProbabilityDistribution{Multivariate})
+<<<<<<< HEAD
     rho = clamp.(exp.(unsafeLogMean(marg_p)), tiny, Inf) # Softens the parameter
     
+=======
+    rho = clamp.(exp.(unsafeLogMean(marg_p)), tiny, huge)
+
+>>>>>>> Allow inference with categorical observations
     Message(Univariate, Categorical, p=rho./sum(rho))
 end
 
